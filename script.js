bec1beeb96c595f98417a7b1a4bdd3fefc110b94
H. Vanity <Hershels.Vanity@gmail.com>
2023-03-19 22:06:38 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+document.querySelector('.message');
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+// Testing
+//Double v
+//will this work
