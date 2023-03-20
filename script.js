0da0062bd331a794c7443938d7128c11de53fb95
H. Vanity <Hershels.Vanity@gmail.com>
2023-03-19 23:01:11 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 'use strict';
 
-document.querySelector('.message');
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = '🎉 Correct Number!';
-// Testing
-//Double v
-//will this work
+// document.querySelector('.message');
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = number;
+
+document.querySelector('.check').addEventListener('.click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔No number!⛔';
+  }
+});
