448b7f2c262d815018be75d7ed5bac4c9328ee41
H. Vanity <Hershels.Vanity@gmail.com>
2023-03-26 20:04:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,8 @@
 // document.querySelector('.message').textContent = '🎉 Correct Number!';
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -24,28 +24,22 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
     document.querySelector('.again').textContent = 'Play Again?';
-    // When guess is too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lose!';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('.again').textContent = 'Play Again?';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
-    // When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lose!';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('.again').textContent = 'Play Again?';
-    }
+
+    // when guess is wrong
+  } else if (guess !== secretNumber) {
+    document.querySelector('.message').textContent =
+      guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else {
+    document.querySelector('.message').textContent = '💥 You lose!';
+    document.querySelector('.score').textContent = 0;
+    document.querySelector('.again').textContent = 'Play Again?';
   }
 });
 
